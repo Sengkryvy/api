76d50d3631131510c7bd03c60e66eda4df1204b5
SengkryVY <sengkryvy@gmail.com>
2019-05-28 00:36:02 +0700

    Adding Review Resource and Pagination for all product

---
+++ app/Http/Resources/Review/ReviewResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Review;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->customer_name,
+            'body' => $this->review,
+            'star' => $this->star,
+        ];
+    }
+}
