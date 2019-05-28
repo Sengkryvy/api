00f922b6cd998e43818c6b6ec0ea7038038dff08
SengkryVY <sengkryvy@gmail.com>
2019-05-29 00:09:50 +0700

    Delete Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -41,7 +41,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function store(ProductRequest $request)
@@ -61,7 +61,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
      * @return ProductResource
      */
     public function show(Product $product)
@@ -73,7 +73,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -84,8 +84,8 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Product  $product
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Model\Product $product
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Product $product)
@@ -101,11 +101,16 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function destroy(Product $product)
     {
-        //
+//        return $product;
+        $product->delete();
+        return response([
+            null,
+        ], Response::HTTP_NO_CONTENT);
     }
 }
